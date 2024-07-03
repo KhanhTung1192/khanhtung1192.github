48b7c0a741d54b3b71acc838016cc3510062d442
vuonglq1993 <lqv1993@gmail.com>
2024-07-03 22:06:36 +0700

    1
    1

---
+++ BTnhom/event2.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import "./staff.css";
+
+const Event2 = () => {
+  return (
+    <div className="row justify-content-center event3">
+      <div className="text-center col-10">
+        <p className="text-center info">STAFF AUGMENTATION SERVICES</p>
+        <h1 className="text-center h1text text-2">
+          Select best-fit talent from our
+          <strong> powerful network of battle-tested IT specialists </strong>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Event2;
